d3fee51cee3d1f62fd821304b19755b5538131b4
고윤혁 <sksgur3217@gmail.com>
2022-06-28 14:29:07 +0900

    Deploy Next.js to gh-pages

---
+++ _next/static/1ZWGNN7a2Mx3K8scMf1XU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c0835a1764e5f778.js"],"/_error":["static/chunks/pages/_error-777a5f0e462a9bdf.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
